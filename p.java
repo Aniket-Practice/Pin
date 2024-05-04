d30d7318f6d0d11e447222c8190aa07486f48dec
Aniket Practice <waghmareaniket150@gmail.com>
2024-05-04 12:06:42 +0530

    Updt

--- p.java
+++ p.java
@@ -3,5 +3,7 @@ package Pinterest;
 public class p {
     public static void main(String[] args) {
         System.out.println("Hello Pinterest!");
+        System.out.println("How may I help you");
     }
 }
+
